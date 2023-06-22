349d810f6b9e7bebeb2111b587a3f61ad8204fd2
Danielflores997 <daniflores5492@gmail.com>
2023-06-22 16:19:37 -0500

    arreglando el php de registro de usuario

---
+++ OneDrive/Documentos/Ferreteria Meissen/compartido/menu.php
@@ -0,0 +1,26 @@
+<?php
+echo '<div class="encabezado">
+<header>
+    <div class = titulo>
+    <h1>FERRETERIA MEISSEN</h1>
+    </div>    
+    <div class="logo" >
+    <img src="../imagenes/ferreteria.jpeg" alt="logo ferreteria">
+    </div>
+</header>
+<nav class="navbar">
+    <div class="lista">
+    <a href="index.html" class="Catalogo">Catalogo</a>
+    <a href="#" class="Pintura">Pintura</a>
+    <a href="elctricas.html" class="Electricas">Electricas</a>
+    <a href="#" class="Herramientas_Manuales">Herramientas Manuales</a>
+    <a href="gesticatalogo.php" class="Accesorios">Accesorios</a>
+    <button class="btn-login">
+    <a class="btn-login"href="loginCliente.html">Acceder</a>
+    </button>
+    <button class="btn-login">
+    <a class="btn-login"href="registroCliente.html">Regístrate</a>
+    </button>
+    </div>
+</div>'
+?>
